b94d7eb1a568ab1f8e4b881762f9512c6425ed11
Jeremy Abbott <jeremymabbott@gmail.com>
2021-11-22 20:10:35 +0000

    Sets up some GitHub actions
    * Makes sure solution builds
    * Publishes Docker images using GitHub container registry
    * Also improves message output a lot

--- src/EndwalkerBot/Program.fs
+++ src/EndwalkerBot/Program.fs
@@ -3,7 +3,6 @@
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open DSharpPlus.CommandsNext
 
 module Program =
     open Bot
@@ -12,19 +11,7 @@ module Program =
         Host.CreateDefaultBuilder(args)
             .ConfigureServices(fun hostContext services ->
                 services.Configure<DiscordOptions>(hostContext.Configuration.GetSection(DiscordOptions.DiscordConfig)) |> ignore
-                services.AddSingleton<CommandsNextConfiguration>(fun svcProvider -> 
-                    let commandsConfig = CommandsNextConfiguration()
-                    commandsConfig.StringPrefixes <- ["!"]
-                    let svcs = ServiceCollection()
-                    svcs.AddSingleton<BotOptions>(fun _ ->
-                        let c = svcProvider.GetService<IConfiguration>()
-                        let botOptions =
-                            c.GetSection(BotOptions.EndwalkerBotConfig).Get<BotOptions>()
-                        botOptions
-                    ) |> ignore
-                    commandsConfig.Services <- svcs.BuildServiceProvider()
-                    commandsConfig
-                ) |> ignore
+                services.Configure<BotOptions>(hostContext.Configuration.GetSection(BotOptions.EndwalkerBotConfig)) |> ignore
                 services.AddHostedService<Tataru>() |> ignore)
     
     [<EntryPoint>]
