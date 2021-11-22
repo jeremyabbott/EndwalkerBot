03eef512c8392e874a1a792d24be7ac32977fbf2
Jeremy Abbott <jeremymabbott@gmail.com>
2021-11-22 20:10:35 +0000

    Sets up some GitHub actions
    * Makes sure solution builds
    * Publishes Docker images using GitHub container registry
    * Also improves message output a lot

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+# Creates a non-root user with an explicit UID and adds permission to access the /app folder
+# For more info, please refer to https://aka.ms/vscode-docker-dotnet-configure-containers
+RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+USER appuser
+
+COPY ./release /app/
+ENTRYPOINT ["dotnet", "EndwalkerBot.dll"]
--- src/EndwalkerBot/EndwalkerBot.fs
+++ src/EndwalkerBot/EndwalkerBot.fs
@@ -3,7 +3,6 @@ namespace WarriorOfLight.EndwalkerBot
 open DSharpPlus.CommandsNext
 open DSharpPlus.CommandsNext.Attributes
 open System
-open System.Linq
 open System.Threading
 open System.Threading.Tasks
 open Microsoft.Extensions.Hosting
@@ -11,14 +10,33 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Options
 open DSharpPlus
 open DSharpPlus.Entities
+open Microsoft.Extensions.DependencyInjection
 
 module Bot =
+    
     let daysUntil (target: DateTime) (now: DateTime) =
         let remainingDays = target - now
         remainingDays
 
     let formatDays (interval: TimeSpan) =
         $"{interval.Days} days, {interval.Hours} hours, and {interval.Minutes} minutes"
+    
+    type FinalDays =
+        { EarlyAccessDate: DateTime
+          EarlyAccessInterval: TimeSpan
+          EarlyAccessTimeStamp: int64
+          ReleaseDate: DateTime
+          ReleaseInterval: TimeSpan
+          ReleaseTimeStamp: int64
+          Now: DateTime } 
+        static member Create(earlyAccessDate, releaseDate, now) =
+            { EarlyAccessDate = earlyAccessDate
+              EarlyAccessInterval = daysUntil earlyAccessDate now 
+              EarlyAccessTimeStamp = DateTimeOffset(earlyAccessDate).ToUnixTimeSeconds()
+              ReleaseDate = releaseDate
+              ReleaseInterval = daysUntil releaseDate now
+              ReleaseTimeStamp = DateTimeOffset(releaseDate).ToUnixTimeSeconds()
+              Now = now }
 
     let messages =
             [|
@@ -27,71 +45,109 @@ module Bot =
                 sprintf "The final days will be upon us in %s"
             |]
     
-    let getMessageString message =
+    let rec getRandomExclusive (random: unit -> int) (exclude: int) =
+        let r = random()
+        if (r = exclude) then getRandomExclusive random exclude
+        else r
+
+    let getMessageStrings () =
         let r = Random()
         let l = messages.Length
-        let n = r.Next(0, l)
-        messages[n] message
+        let f = r.Next(0, l)
+        let s = getRandomExclusive (fun _ -> r.Next(0, l)) f
+        messages[f], messages[s]
     
-    let buildEmbed (earlyAccessInterval: TimeSpan) (releaseInterval:TimeSpan) =
+    /// https://discord.com/developers/docs/reference#message-formatting-formats
+    let getRelativeEpochTag (ts: int64) =
+        $"<t:{ts}:R>"
+
+    /// https://discord.com/developers/docs/reference#message-formatting-formats
+    let getFullDateTimeEpochTag (ts: int64) =
+        $"<t:{ts}:F>"
+
+    let buildEmbed (finalDays: FinalDays) =
+        let (firstMessage, secondMessage) = getMessageStrings ()
         let earlyAccessMessage =
-            earlyAccessInterval
-            |> formatDays
-            |> getMessageString
+            let formattedDate = 
+                finalDays.EarlyAccessTimeStamp
+                |> getFullDateTimeEpochTag
+            let relativeDate =
+                finalDays.EarlyAccessTimeStamp
+                |> getRelativeEpochTag
+            let cuteMessage =
+                finalDays.EarlyAccessInterval
+                |> formatDays
+                |> firstMessage
+            $"{formattedDate}{Environment.NewLine}{relativeDate}{Environment.NewLine}{cuteMessage}"
         let releaseMessage =
-            releaseInterval
-            |> formatDays
-            |> getMessageString
-        let botMessage = $"EARLY ACCESS: {earlyAccessMessage}{Environment.NewLine}LAUNCH: {releaseMessage}{Environment.NewLine}"
+            let formattedDate = 
+                finalDays.ReleaseTimeStamp
+                |> getFullDateTimeEpochTag
+            let relativeDate =
+                finalDays.ReleaseTimeStamp
+                |> getRelativeEpochTag
+            let cuteMessage =
+                finalDays.ReleaseInterval
+                |> formatDays
+                |> secondMessage
+            $"{formattedDate}{Environment.NewLine}{relativeDate}{Environment.NewLine}{cuteMessage}"
+
         let eb = DiscordEmbedBuilder()
         eb
-            .WithDescription(botMessage)
             .WithTitle("Time Until the Final Days")
+            .AddField("Early Access", earlyAccessMessage)
+            .AddField("Offical Launch", releaseMessage)
+            .WithTimestamp(finalDays.Now)
 
-    let sendFinalDaysMessage (earlyAccessInterval: TimeSpan) (releaseInterval:TimeSpan) (ctx: CommandContext) = 
+    let sendFinalDaysMessage (finalDays: FinalDays) (ctx: CommandContext) = 
         task {
             do! ctx.TriggerTypingAsync()
-            let embed = buildEmbed earlyAccessInterval releaseInterval
+            let embed = buildEmbed finalDays
             let! _ = 
                 ctx.RespondAsync(embed)
             return ()
         } :> Task
 
     type BotOptions() =
+        let earlyAccessDefault = DateTime(2021, 12, 3, 9, 0, 0)
+        let releaseDefault = DateTime(2021, 12, 7, 9, 0, 0)
         static member EndwalkerBotConfig = "EndwalkerBotConfig"
-        member val EarlyAccessDate = DateTime.UtcNow with get, set
-        member val ReleaseDate = DateTime.UtcNow with get, set
+        
+        member val EarlyAccessDate = earlyAccessDefault with get, set
+        member val ReleaseDate = releaseDefault with get, set
 
     type EndwalkerBot(options: BotOptions) =
         inherit BaseCommandModule ()
         
         let botOptions = options
         
-        let daysUntilEarlyAccess now = daysUntil botOptions.EarlyAccessDate now
-        let daysUntilLaunch now = daysUntil botOptions.ReleaseDate now
-
         [<Command "finalDays">]
         member _.FinalDays(ctx: CommandContext) = 
-            let now = DateTime.UtcNow // todo timezones
+            let now = DateTime.UtcNow 
             
-            let earlyAccess = 
-                now
-                |> daysUntilEarlyAccess
+            let finalDays = FinalDays.Create(botOptions.EarlyAccessDate, botOptions.ReleaseDate, DateTime.UtcNow)
                 
-            let launch = 
-                now
-                |> daysUntilLaunch
-                
-            sendFinalDaysMessage earlyAccess launch ctx
+            sendFinalDaysMessage finalDays ctx
+    
+    let buildCommandsConfig (botOptions: BotOptions) =
+        let commandsConfig = CommandsNextConfiguration()
+        commandsConfig.StringPrefixes <- ["!"]
+        let svcs = ServiceCollection()
+        svcs.AddSingleton<BotOptions>(fun _ -> botOptions) |> ignore
+        commandsConfig.Services <- svcs.BuildServiceProvider()
+        commandsConfig
 
 type DiscordOptions() =
     static member DiscordConfig = "DiscordConfig"
     member val BotToken = "" with get, set
 
-type Tataru(logger: ILogger<Tataru>, options: IOptions<DiscordOptions>, cmdsConfig: CommandsNextConfiguration) =
+type Tataru(logger: ILogger<Tataru>, options: IOptions<DiscordOptions>, botOptions: IOptions<Bot.BotOptions>) =
     inherit BackgroundService()
+    
+    do logger.LogInformation("Starting: {time}", DateTimeOffset.Now)
     let discordConfig = DiscordConfiguration(Token=options.Value.BotToken, TokenType=TokenType.Bot)
-    let commandsConfig = cmdsConfig
+    do logger.LogInformation($"botOptions: EarlyAccessDate {botOptions.Value.EarlyAccessDate}")
+    let commandsConfig = Bot.buildCommandsConfig botOptions.Value
     let discordClient = new DiscordClient(discordConfig)
     let commands = discordClient.UseCommandsNext(commandsConfig)
     do commands.RegisterCommands<Bot.EndwalkerBot>()
@@ -100,6 +156,5 @@ type Tataru(logger: ILogger<Tataru>, options: IOptions<DiscordOptions>, cmdsConf
         task {
             do! discordClient.ConnectAsync()
             while not ct.IsCancellationRequested do
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now)
                 do! Task.Delay(1000)
         } :> Task
